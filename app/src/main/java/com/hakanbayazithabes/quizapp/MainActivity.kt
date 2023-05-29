b55e5055934e1118cc1ad3db8a13633a3e9b7adc
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-05-29 15:38:44 +0300

    Added creating the question Model

--- app/src/main/java/com/hakanbayazithabes/quizapp/MainActivity.kt
+++ app/src/main/java/com/hakanbayazithabes/quizapp/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.hakanbayazithabes.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hakanbayazithabes.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root // view is the root of the layout
+        setContentView(view)
+
+        binding.btnStart.setOnClickListener{
+            if (binding.etName.text.toString().isEmpty()){
+                binding.etName.error = "Please enter your name"
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
